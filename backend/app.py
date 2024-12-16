cd640066dc5e6d1de62d947d0caeaae27935b4a5
desa-thomas <desa2thomas@gmail.com>
2024-12-16 17:10:30 -0500

    created test endpoints

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def test():
     return {'message': 'testing'}
 
+
+@app.route('/dataframe/<dataframe>')
+def get_dateframe(dataframe):
+    """
+    Get a specific dataframe formatted as a json
+    """
+    match dataframe:
+        case 'spending-by-category':
+            return {'data': 'good category'}, 200
+        
+        case _:
+            return {'error': f'{dataframe} is not a valid dataframe'}, 400
+        
+    return
+
+
 if __name__ == '__main__':
     app.run(debug=True)
